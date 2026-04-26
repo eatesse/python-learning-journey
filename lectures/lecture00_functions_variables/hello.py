3fc810f09ae26261debf823d8d23a5a8be9e5e90
eatesse <elyseetessenana@gmail.com>
2026-04-26 18:43:22 +0100

    Initial commit

---
+++ lectures/lecture00_functions_variables/hello.py
@@ -0,0 +1,9 @@
+# Ask the user for their name
+name = input('What is your name? ')
+
+
+# Remove any leading or trailing whitespace from the name
+name = name.strip()
+
+# Print a greeting to the user
+print(f"hello, {name}")
